ef2f92f91467937043c3a27451709cd80f9f2fed
A. Aladdin <a.aladdin.developer@gmail.com>
2022-12-28 09:58:19 +0200

    removed unnecessary components and implemented the header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'first-4-proj';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
-import { ServerComponent } from './server/server.component';
-import { WarningComponent } from './warning/warning.component';
-import { SuccessComponent } from './success/success.component';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningComponent,
-    SuccessComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, NgbModule],
+  imports: [BrowserModule, FormsModule],
 })
 export class AppModule {}
 console.log('working');
