1f0eb56c46602d4084ff114c649e35d6fead0136
A. Aladdin <a.aladdin.developer@gmail.com>
2022-12-31 23:53:17 +0200

    Implemented the shopping list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../Shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent {}
+export class ShoppingListComponent {
+  Ingredients: Ingredient[] = [
+    new Ingredient('tomatos', 5),
+    new Ingredient('potatoes', 15),
+    new Ingredient('carrots', 25),
+  ];
+}
