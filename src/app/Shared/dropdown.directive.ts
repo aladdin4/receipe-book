b50187c1c1503c03ae156fb4e511ec1892647f5c
A. Aladdin <a.aladdin.developer@gmail.com>
2023-01-26 15:24:10 +0200

    completed the open/close dropdown directive

--- src/app/Shared/dropdown.directive.ts
+++ src/app/Shared/dropdown.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appDropdown]'
+  selector: '[appDropdown]',
 })
 export class DropdownDirective {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  constructor() { }
+  @HostListener('click') toggleDropDown() {
+    let menu = this.el.nativeElement.children[1];
 
+    if (menu.classList.value.includes('show')) {
+      this.renderer.removeClass(menu, 'show');
+    } else {
+      this.renderer.addClass(menu, 'show');
+    }
+  }
 }
